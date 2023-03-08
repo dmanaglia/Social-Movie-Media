1e1a65ac805bde8d2bc0040f0fd316fd3040b68e
@hudabarry-wwMZPT <hudabarry@yahoo.com>
2023-03-08 11:59:02 -0500

    add models

---
+++ models/index.js
@@ -0,0 +1,22 @@
+const User = require('./User');
+const Review = require('./Review');
+
+Review.belongsTo(User, {
+  foreignKey: 'userId',
+  onDelete: 'CASCADE'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'postId',
+  onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'userId',
+  onDelete: 'CASCADE'
+});
+
+module.exports = {
+  User,
+  Review
+};
