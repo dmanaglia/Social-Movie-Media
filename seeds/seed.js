ab1719fd23fa1e9c057d5f4fd35c97e6e7b48888
jmulcahy92 <jacobkmulcahy@gmail.com>
2023-03-08 14:40:27 -0600

    seeds

---
+++ seeds/seed.js
@@ -0,0 +1,31 @@
+const sequelize = require('../config/config');
+const { User, Review, Movie } = require('../models');
+
+const userData = require('./userData.json');
+const movieData = require('./postData.json');
+const reviewData = require('./reviewData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  const movies = await Movie.bulkCreate(movieData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  for (const review of reviewData) {
+    await Review.create({
+      ...review,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
+
+  process.exit(0);
+};
+
+seedDatabase();
